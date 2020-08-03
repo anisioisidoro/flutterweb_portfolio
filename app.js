f236ddab92932cc31c14c6454041bb1d008ce1bf
Anísio Isidoro Gomes <anisio.gomes@digitalfactory.co.ao>
2020-08-03 13:00:03 +0100

    save

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public-flutter')));
+app.use(express.static(path.join(__dirname, 'flutter')));
 
 
 module.exports = app;
